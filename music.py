7cb0215cd9a404df3fb5dbb1655461b92f554474
Khlmood <khalil.s.moawad@net.usek.edu.lb>
2023-02-04 17:51:36 +0200

    made some design changes

--- music.py
+++ music.py
@@ -1,5 +1,4 @@
 import customtkinter
-import subprocess
 
 customtkinter.set_appearance_mode("dark") 
 customtkinter.set_default_color_theme("dark-blue")
@@ -10,28 +9,27 @@ class App(customtkinter.CTk):
 
         self.path = "C:\\Users\\User\\Music"
 
-        self.geometry("600x400")
+        self.geometry("730x380")
         self.title("Download Music")
         self.minsize(300, 200)
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
-
         self.textbox = customtkinter.CTkTextbox(master=self)
-        self.textbox.grid(row=0, column=0, columnspan=3, padx=20, pady=(20, 0))
+        self.textbox.pack(side='top', padx=20, pady=(20, 0))
 
-        self.button_search = customtkinter.CTkButton(master=self, text="Seach", command=self.search)
-        self.button_search.grid(row=2, column=2, padx=20, pady=20)
+        self.frame = customtkinter.CTkFrame(self)
+        self.frame.pack(side='bottom')
 
-        self.button_download = customtkinter.CTkButton(master=self, text="Download", command=self.download)
-        self.button_download.grid(row=1, column=2, padx=20, pady=20)
+        self.button_change_path = customtkinter.CTkButton(master=self.frame, text="Change path", command=self.create_toplevel)
+        self.button_change_path.pack(side='left', padx=20, pady=20)
 
-        self.button_change_path = customtkinter.CTkButton(master=self, text="Change path", command=self.create_toplevel)
-        self.button_change_path.grid(row=1, column=0, padx=20, pady=20)
+        self.button_download = customtkinter.CTkButton(master=self.frame, text="Download", command=self.download)
+        self.button_download.pack(side='right', padx=20, pady=20)
 
-        self.entry = customtkinter.CTkEntry(master=self, placeholder_text="Search Artist")
-        self.entry.grid(row=2, column=0, columnspan=2, padx=20, pady=20)
+        self.button_search = customtkinter.CTkButton(master=self.frame, text="Seach", command=self.search)
+        self.button_search.pack(side='right', padx=20, pady=20)
+
+        self.entry = customtkinter.CTkEntry(master=self.frame, placeholder_text="Search Artist")
+        self.entry.pack(side='left', padx=20, pady=20)
         
     def download(self):
         self.textbox.insert("insert", self.entry.get() + "\n")
@@ -47,14 +45,15 @@ class App(customtkinter.CTk):
         self.toplevel.geometry("380x80")
 
         entry = customtkinter.CTkEntry(master=self.toplevel, placeholder_text=self.path)
-        entry.grid(row=0, column=0, columnspan=2, padx=20, pady=20)
+        entry.pack(side='left', padx=20, pady=20)
 
         button = customtkinter.CTkButton(master=self.toplevel, text="Apply", command=lambda: self.change_path(entry))
-        button.grid(row=0, column=2, padx=20, pady=20)
+        button.pack(side='right', padx=20, pady=20)
 
 
     def change_path(self, entry):
-        self.path = entry.get()
+        if entry.get() != "":
+            self.path = entry.get()
         self.toplevel.destroy()
 
 if __name__ == "__main__":
